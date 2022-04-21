aa300d9994b85374192d71b25c4bc033994cac8a
tharonweighill <tharon@fusionauth.io>
2022-04-21 15:51:22 -0600

    ADD react routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import "./styles.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Todos from "./Todos";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+          <Switch>
+              <Route exact path="/" children={<Todos />} />
+              <Route path="/todos/:id" children={<Todos />} />
+          </Switch>
+
+      </Router>
     </div>
   );
 }
