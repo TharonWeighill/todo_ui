498c738270cd67318cbe167d865b5aab5ee1ef83
tharonweighill <tharon@fusionauth.io>
2022-04-21 15:39:36 -0600

    ADD todo/todos components

---
+++ src/Todo.jsx
@@ -0,0 +1,8 @@
+import React from 'React'
+
+const Todo = () => {
+    return (
+        <p> This is a Todo component </p>
+    )
+};
+export default Todo;
---
+++ src/Todos.jsx
@@ -0,0 +1,8 @@
+import React from 'React'
+
+const Todos = () => {
+    return (
+        <p> This is a Todos component </p>
+    )
+};
+export default Todos;
