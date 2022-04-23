106c01f6f1ceeb197373a8948fd961e9ae92870d
tharonweighill <tharon@fusionauth.io>
2022-04-23 12:30:09 -0600

    ADD form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,14 +1,19 @@
 import React, {useState} from 'react';
 
-function TodoForm() {
-    const [input, setState] = useState('');
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+
+    const handleChange = e => {
+        setInput(e.target.value);
+    };
 
     const handleSubmit = e => {
         e.preventDefault();
         props.onSubmit({
             id: Math.floor(Math.random() * 10000),
             text: input
-        })
+        });
+        setInput('')
     };
 
     return (
@@ -18,6 +23,7 @@ function TodoForm() {
                 placeholder="Add a todo"
                 value={input} name="text"
                 className="todo-input"
+                onChange={handleChange}
             />
             <button className='todo-button'>Add todo</button>
         </form>
