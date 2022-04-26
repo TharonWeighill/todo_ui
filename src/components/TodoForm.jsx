ae750dc24093ff380446c5450b8a35f04eb6a9b5
tharonweighill <tharon@fusionauth.io>
2022-04-26 15:32:25 -0600

    ADD fetch post request

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,32 +1,41 @@
-import React, {useState} from 'react';
+import React, {Component, useState} from 'react'
+import axios from "axios";
 
-function TodoForm(props) {
-    const [input, setInput] = useState('');
-
-    const handleChange = e => {
-        setInput(e.target.value);
-    };
-
-    const handleSubmit = e => {
-        e.preventDefault();
-        props.onSubmit({
-            id: Math.floor(Math.random() * 10000),
-            text: input
-        });
-        setInput('')
-    };
-
-    return (
-        <form className='todo-form' onSubmit={handleSubmit}>
-            <input
-                type="text"
-                placeholder="Add a todo"
-                value={input} name="text"
-                className="todo-input"
-                onChange={handleChange}
-            />
-            <button className='todo-button'>Add todo</button>
-        </form>
-    )
+export default class TodoForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            listName: '',
+            listBody: ''
+        }
+    }
+    changeHandler = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+    submitHandler = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+        axios.post('http://localhost:9900/api/lists', this.state)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {console.log(error)
+            })
+    }
+    render () {
+        const { listName, listBody } = this.state;
+        return (
+            <div>
+                <form onSubmit={this.submitHandler}>
+                    <div>
+                        <input type="text" name="listName" value={listName} placeholder="List Name" onChange={this.changeHandler}/>
+                    </div>
+                    <div>
+                        <input type="text" name="listBody" value={listBody} placeholder="List Body" onChange={this.changeHandler}/>
+                    </div>
+                    <button type="submit">Create Todo List</button>
+                </form>
+            </div>
+        )
+    }
 }
-export default TodoForm
