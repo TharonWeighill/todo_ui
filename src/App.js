d30aa7e2e911fe956eec13faa22e9cc9f0e62450
tharonweighill <tharon@fusionauth.io>
2022-04-27 10:49:50 -0600

    ADD proper routing

--- src/App.js
+++ src/App.js
@@ -11,14 +11,13 @@ import ErrorPage from "./components/ErrorPage";
 function App() {
   return (
       <div className='todo-app'>
-          <h1>Welcome!</h1>
+          <h1>This is a Todo App that uses FusionAuth and CleanSpeak</h1>
               <Routes>
-                  <Route path="/home" element={Home} />
-                  <Route path="/login" element={FusionAuthLogin} />
-                  <Route path="/new_todo" element={TodoForm} />
-                  <Route path="/todo_lists" element={FetchTodoLists} />
-                  <Route path="/error" element={ErrorPage} />
-
+                  <Route path="/home" element={<Home />} />
+                  <Route path="/login" element={<FusionAuthLogin />} />
+                  <Route path="/new_todo" element={<TodoForm />} />
+                  <Route path="/todo_lists" element={<FetchTodoLists />} />
+                  <Route path="*" element={<ErrorPage />} />
               </Routes>
     </div>
   );
