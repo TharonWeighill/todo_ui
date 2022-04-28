e7282c2aa223edc5ad470855c5f8183f2f060eeb
tharonweighill <tharon@fusionauth.io>
2022-04-27 23:18:54 -0600

    ADD styling REFRACTOR

--- src/App.js
+++ src/App.js
@@ -7,19 +7,40 @@ import TodoForm from './components/TodoForm';
 import FetchTodoLists from "./components/TodoLists";
 import Navbar from "./components/NavBar";
 import Home from "./components/Home";
+import React from "react";
 
+const logo = require('./images/gray-orange-logo-with-tagline.png');
+const logo2 = require('./images/cleanspeak-horizontal.png')
 function App() {
   return (
       <div className='todo-app'>
+          <div className='logo1'>
+              <img src={logo}/>
+          </div>
+          <div className='logo2'>
+              <img src={logo2}/>
+          </div>
+
           <Navbar />
-          <h1 className='todo-header'>This is a Todo App that uses<div className='FA'>FusionAuth</div><div>And</div><div className='CS'>CleanSpeak</div></h1>
-              <Routes>
-                  <Route path="/home" element={<Home />} />
-                  <Route path="/new_todo" element={<TodoForm />} />
-                  <Route path="/todo_lists" element={<FetchTodoLists />} />
-                  <Route path="*" element={<ErrorPage />} />
-              </Routes>
+          <h1 className='todo-header'>This is a Todo App that uses
+              <div className='FA'>
+                  <a href='https://fusionauth.io/'>FusionAuth</a>
+              </div>
+              <div>
+                  And
+              </div>
+              <div className='CS'>
+                  <a href='https://cleanspeak.com/'>CleanSpeak</a>
+              </div>
+          </h1>
+          <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/new_todo" element={<TodoForm />} />
+              <Route path="/todo_lists" element={<FetchTodoLists />} />
+              <Route path="*" element={<ErrorPage />} />
+          </Routes>
     </div>
-  );
+  )
 }
 export default App;
+
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
+
+
+const logo = require('../images/gray-orange-logo-with-tagline.png');
+const logo2 = require('../images/cleanspeak-horizontal.png')
+
 export default class NavBar extends React.Component {
     render() {
         return (
             <ul className="navMenu">
-                <img src="gray-orange-logo-with-tagline.png"/>
                 <a href='http://localhost:3000/home'>Home</a>
                 <a href='http://localhost:9011/oauth2/authorize?client_id=a7f398aa-8337-4112-94a0-f99bc29a8136&response_type=code&redirect_uri=http://localhost:3000/home'>Login</a>
                 <a href='http://localhost:9011/oauth2/logout?client_id=a7f398aa-8337-4112-94a0-f99bc29a8136'>Logout</a>
                 <a href='http://localhost:3000/new_todo'>New Todo</a>
                 <a href='http://localhost:3000/todo_lists'>My Todos</a>
-                <div className="dot"></div>
             </ul>
         )
     }
